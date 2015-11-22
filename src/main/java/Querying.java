dc06b59b59ededb64ee43ba57ea295a0ddf97036
ichakid <choirunnisa.fatima@gmail.com>
2015-11-23 06:45:08 +0700

    implement register, login, isUserExist

--- src/main/java/Querying.java
+++ src/main/java/Querying.java
@@ -1,12 +1,18 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
+import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
 import com.mongodb.MongoClient;
 import com.mongodb.ServerAddress;
 
 public class Querying {
 	private DB db;
 	private MongoClient mongoClient;
+	private DBCollection users;
 	
 	public static final String USERLINE = "userline";
 	public static final String TIMELINE = "timeline";
@@ -19,11 +25,26 @@ public class Querying {
                 new ServerAddress("167.205.35.22", 27019)));
 
 		db = mongoClient.getDB("ichayyu");
+		users = db.getCollection("users");
 	}
 	
-	public void register (String username, String password) {}
+	public void register (String username, String password) {
+		BasicDBObject user = new BasicDBObject();
+		user.put("username", username);
+		user.put("password", password);
+		users.insert(user);
+	}
 	
-	public boolean login (String username, String password) {return true;}
+	public boolean login (String username, String password) {
+		BasicDBObject andQuery = new BasicDBObject();
+		List<BasicDBObject> obj = new ArrayList<BasicDBObject>();
+		obj.add(new BasicDBObject("username", username));
+		obj.add(new BasicDBObject("password", password));
+		andQuery.put("$and", obj);
+
+		DBCursor cursor = users.find(andQuery);
+		return cursor.hasNext();
+	}
 	
 	public void follow (String username, String friend) {}
 	
@@ -35,7 +56,12 @@ public class Querying {
 		mongoClient.close();
 	}
 	
-	public boolean isUserExist (String username) {return true;}
+	public boolean isUserExist (String username) {
+		BasicDBObject whereQuery = new BasicDBObject();
+		whereQuery.put("username", username);
+		DBCursor cursor = users.find(whereQuery);
+		return cursor.hasNext();
+	}
 	
 	public boolean isUserFollows (String user1, String user2) {return true;}
 }
