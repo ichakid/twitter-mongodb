39498b3e0a7700c042fc4899f34a17f1c68b37a9
ichakid <choirunnisa.fatima@gmail.com>
2015-11-23 10:33:14 +0700

    implement tweet, follow, isUserFollows

--- src/main/java/Querying.java
+++ src/main/java/Querying.java
@@ -1,38 +1,46 @@
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
+import com.mongodb.BasicDBList;
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
-import com.mongodb.ServerAddress;
 
 public class Querying {
 	private DB db;
 	private MongoClient mongoClient;
 	private DBCollection users;
+	private DBCollection tweets;
 	
 	public static final String USERLINE = "userline";
 	public static final String TIMELINE = "timeline";
 
 	@SuppressWarnings("deprecation")
 	public Querying() {
-		mongoClient = new MongoClient(Arrays.asList(new ServerAddress("167.205.35.19", 27017),
-                new ServerAddress("167.205.35.20", 27018),
-                new ServerAddress("167.205.35.21", 27019),
-                new ServerAddress("167.205.35.22", 27019)));
-
+		mongoClient = new MongoClient("167.205.35.19", 27017);
 		db = mongoClient.getDB("ichayyu");
 		users = db.getCollection("users");
+		tweets = db.getCollection("tweets");
+	}
+	
+	public void close() {
+		mongoClient.close();
 	}
 	
 	public void register (String username, String password) {
-		BasicDBObject user = new BasicDBObject();
-		user.put("username", username);
-		user.put("password", password);
-		users.insert(user);
+		if (!isUserExist(username)) {
+			BasicDBObject user = new BasicDBObject();
+			user.put("username", username);
+			user.put("password", password);
+			users.insert(user);
+		} else {
+			System.out.println("User already exists.");
+		}
 	}
 	
 	public boolean login (String username, String password) {
@@ -46,22 +54,74 @@ public class Querying {
 		return cursor.hasNext();
 	}
 	
-	public void follow (String username, String friend) {}
+	public void follow (String username, String friend) {
+		if (isUserExist(username) && isUserExist(friend)) {
+			BasicDBObject searchQuery = new BasicDBObject("username", friend);
+			Date now = new Date();
+			
+			BasicDBObject follower = new BasicDBObject();
+			follower.put("follower", username);
+			follower.put("since", now);
+			users.update(searchQuery, new BasicDBObject(
+					"$push", new BasicDBObject("followers", follower)));
+			
+			BasicDBObject friendObj = new BasicDBObject();
+			friendObj.put("friend", username);
+			friendObj.put("since", now);
+			users.update(searchQuery, new BasicDBObject(
+					"$push", new BasicDBObject("followers", follower)));
+		} else {
+			System.out.println("Cannot follow " + friend);
+		}
+	}
 	
-	public void tweet (String username, String body) {}
+	public void tweet (String username, String body) {
+		if (isUserExist(username)) {
+			UUID tweet_id = UUID.randomUUID();
+			BasicDBObject tweet = new BasicDBObject();
+			tweet.put("tweet_id", tweet_id);
+			tweet.put("username", username);
+			tweet.put("body", body);
+			tweet.put("time", new Date());
+			tweets.insert(tweet);
+			
+			BasicDBObject tweet2 = new BasicDBObject("tweet_id", tweet_id);
+			insertTweet(new BasicDBObject("userline", tweet2), username);
+			
+			DBCursor cursor = users.find(new BasicDBObject("username", username));
+			if (cursor.hasNext()) {
+				BasicDBList followers = (BasicDBList) cursor.next().get("followers");
+				if (!followers.isEmpty()) {
+					for (Object follower: followers) {
+						insertTweet(new BasicDBObject("timeline", tweet2),
+								(String) ((DBObject) follower).get("follower"));
+					}
+				}
+			}
+		}
+	}
+	
+	public void insertTweet (BasicDBObject tweet, String username) {
+		BasicDBObject searchQuery = new BasicDBObject("username", username);
+		users.update(searchQuery, new BasicDBObject(
+				"$push", new BasicDBObject(tweet)));
+	}
 	
 	public void showTweets (String username, String type) {}
 	
-	public void close() {
-		mongoClient.close();
-	}
-	
 	public boolean isUserExist (String username) {
-		BasicDBObject whereQuery = new BasicDBObject();
-		whereQuery.put("username", username);
+		BasicDBObject whereQuery = new BasicDBObject("username", username);
 		DBCursor cursor = users.find(whereQuery);
 		return cursor.hasNext();
 	}
 	
-	public boolean isUserFollows (String user1, String user2) {return true;}
+	public boolean isUserFollows (String user1, String user2) {
+		List<BasicDBObject> obj = new ArrayList<BasicDBObject>();
+		obj.add(new BasicDBObject("username", user2));
+		obj.add(new BasicDBObject("followers.follower", user1));
+		BasicDBObject andQuery = new BasicDBObject("$and", obj);
+
+		DBCursor cursor = users.find(andQuery);
+		return cursor.hasNext();
+	}
 }
